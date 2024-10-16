5d1222323f951c6a679b1b545fcef806c0c30571
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2024-10-16 19:42:36 +0500

    lecture -02 javascript output, statement, and syntax

---
+++ lecture-02/README.md
@@ -0,0 +1,11 @@
+### Lecture - 02 Javascript Basics
+
+Javascript Output,
+
+# alert("to display output in a popup")
+
+Javascript Statement, 
+
+JavaScript Syntax
+
+
---
+++ lecture-02/script.js
@@ -0,0 +1,29 @@
+// javascript output
+
+// alert("Hello World!");
+
+// console.log("Welcome to EcmaScript");
+
+// document.write("Hi I am from Javascript!");
+
+// document.getElementById("main-heading").innerText="Something Else";
+
+// JavaScript Syntax
+// var productPrice = 10
+
+// JavaScript Variables
+
+// JavaScript uses the keywords var, let and const to declare variables.
+
+// JavaScript Comments
+
+
+//
+
+// Multi-line Comments
+// Multi-line comments start with /* and end with */.
+
+/* ajsdlfjaklsdfj lkajsdlfjlasjdf
+ahsdfhkasdf
+
+*/
