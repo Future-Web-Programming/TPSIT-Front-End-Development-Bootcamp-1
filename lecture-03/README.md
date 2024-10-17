0c555a6008b2debc890967ee1c0b25c86bc1bb2c
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2024-10-17 19:38:37 +0500

    lecture 03 Javascript Variables

---
+++ lecture-03/README.md
@@ -0,0 +1,8 @@
+# lecture -03 Javscript Variables
+
+we have learned about Javascript Variables in Detail, 
+var , let and const. And Difference between let and const also we have studied which one to use.
+
+## Assingment:
+
+Practice which we have learnt
---
+++ lecture-03/script.js
@@ -0,0 +1,49 @@
+// Javscrript Varriables
+
+// document 
+
+// var 
+// let 
+// const
+
+// var price = 10;
+
+// console.log(price)
+
+// let shoppingCart = "2 Itesm";
+
+// document.write("Your Shopping cart is Empty: ",shoppingCart)
+
+// document.write(`Your cart is: ${shoppingCart}`)
+
+// const PI = 3.14
+
+// console.log(PI)
+
+// var price = 20
+// var price = 30
+// document.write(price)
+
+// let price = 20;
+// let totalPrice = 40;
+// console.log(price)
+// price = 40;
+
+// const prodcut1 = 30;
+// const prodcut2 = 200;
+// let totalPrice = prodcut1 + prodcut2;
+// console.log(totalPrice)
+
+// JavaScript Identifiers
+
+// do 
+// const username = "ghazi";
+// const total_price = 20;
+// const studentName = "Muhammad";
+// const $fruits = ['santra', 'malta'];
+// const _total = 20;
+
+// don't
+// const function = 20;
+
+
