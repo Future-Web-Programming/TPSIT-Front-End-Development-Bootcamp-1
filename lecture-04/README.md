f754cd71bed9701230ec7d4bc95a7b8f5a6eb47d
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2024-10-22 19:41:38 +0500

    lecture -04 Variable Scope and Operators

---
+++ lecture-04/README.md
@@ -0,0 +1,9 @@
+# Lecture -04 Variables Scope and Operators.
+
+In this Chapter we have studied about Variables Scope and Operators in Detail.
+
+# Assingment
+
+You guys have to practice all.
+
+
---
+++ lecture-04/script.js
@@ -0,0 +1,189 @@
+// let username = "Yahya";
+
+
+// {
+//     var username = "Ghazi Orhan";
+// }
+// console.log(username)
+
+// function welcome (){
+//     var username = "Ghazi Orhan";
+//     console.log(username)
+// }
+// welcome()
+// console.log(username)
+
+
+// function welcome () {
+//     let username = "yahya";
+//     console.log(`Welcoome Mr: ${username}`)
+// }
+// welcome()
+
+
+
+// let totalPrice = 20;
+//  totalPrice = 24;
+
+// console.log(totalPrice)
+
+// const price = 20;
+// price = 30;
+// console.log(price)
+
+// {
+//     const price = 20;
+// }
+// console.log(price)
+
+// JavaScript Operators
+
+
+// The Assignment Operator = assigns values
+
+// The Addition Operator + adds values
+
+// The Multiplication Operator * multiplies values
+
+// The Comparison Operator > compares values
+
+// // addition 
+// let price = 20;
+
+// console.log(price + 20)
+
+
+// multiplication
+
+// let price = 20;
+// let tax = 10;
+// totalPrice = price * tax;
+// console.log(totalPrice)
+
+// substraction
+
+// let price = 20;
+// let tax = 10;
+// totalPrice = price - tax;
+// console.log(totalPrice)
+
+
+// remainder
+
+// let price = 20;
+// let tax = 10;
+// totalPrice = price % tax;
+// console.log(totalPrice)
+
+
+
+// comparison operator
+
+// ==	equal to
+
+// let price = 20;
+
+// console.log(price == '20');
+
+// ===	equal value and equal type
+
+// let price = 20;
+
+// console.log(price === '20');
+
+// !=	not equal
+
+// let price = 20;
+// console.log(price != '21');
+
+
+// !==	not equal value or not equal type
+
+// let price = 20;
+
+// console.log(price !== '20');
+
+
+// >	greater than
+
+// <	less than
+
+// >=	greater than or equal to
+// let price = 20;
+
+// console.log(price >= 21);
+// <=	less than or equal to
+// let price = 20;
+// console.log(price <= 15);
+// ?	ternary operator
+
+
+
+// let price = 20;
+// let tax = 10;
+// totalPrice = price * tax;
+// console.log(totalPrice < 10)
+
+// Exponentiation 
+
+// let price = 2;
+// let tax = 2;
+// totalPrice = price * tax;
+// console.log(totalPrice ** 2)
+
+// increment operrrator
+// let price = 20;
+
+// console.log(price++);
+// console.log(price);
+
+// decrement operator
+
+// addition Assignment Operator (+=) 
+
+
+// let price = 20;
+
+// // let totalPrice = price + 5;
+// // price = price + 5;
+// // price -= 5;
+
+// console.log(price)
+// console.log(price)
+// console.log(price)
+// console.log(price)
+// console.log(price)
+
+
+
+
+// string addtion
+
+// console.log("Hi " + "Welcoome");
+
+// let username = 'Orhan';
+// console.log(`Hi How are you ? ${username}`)
+
+// Adding Strings and Numbers
+
+// let price = '20';
+// let username = 'Ghazi';
+
+// // console.log(`Here is the Answer ${price} ${username}`)
+
+// console.log(price + username);
+// console.log(typeof(price))
+
+
+// JavaScript Logical Operators
+
+// logical and &&
+let price = 20
+
+let totalPrice = 21
+
+console.log(price && totalPrice == 20)
+
+// logical or
+
+// 
