f967dafaa9a99321b73e492cd186d43699dbe40a
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2024-10-23 19:27:59 +0500

    lecture -05 Javascript Data Types

---
+++ lecture-05/README.md
@@ -0,0 +1,7 @@
+# lecture -05 JavaScript Data Types
+
+we have studied about 8 data types in js, like number, string, array, object, bigint , boolean, undefined, null.
+
+# Assingment
+
+Try to cover all of your previous lectures and also this lecture as well. you guys will show me assignment.
---
+++ lecture-05/script.js
@@ -0,0 +1,59 @@
+// JavaScript Data Types
+
+
+
+// int / number
+
+// const rollNumber = 20;
+
+// console.log(typeof(rollNumber))
+
+// string data type
+
+// const userName = 'Raja';
+
+// console.log(typeof userName)
+// Bigint
+
+// let number = 123e10;
+// console.log(number)
+
+// let bigNumber = BigInt(123456789012345678901234567890);
+
+// console.log(bigNumber)
+// Boolean
+// true / false 
+
+// let userLogined = false;
+// console.log(userLogined==false);
+// console.log('Welcome:')
+
+// Undefined
+
+// let userMarks = undefined;
+
+// console.log(typeof userMarks);
+// Null
+
+// let marks = null;
+// marks = 20;
+// console.log(typeof marks)
+
+// Symbol
+
+// Object
+
+const person = {
+    firstName: "Ghazi",
+    lastName: 'Orhan',
+    rollNumber: 20,
+    status: true,
+}
+
+console.log(person.rollNumber);
+
+// Arrays
+
+// let fruits = ["Mango","Orange","Naspati"];
+
+// console.log(fruits[2])
