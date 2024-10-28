3c1839c926be4d101f77ec36e84dc91161f37135
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2024-10-28 19:54:11 +0500

    lecture -07 Javascript Objects

---
+++ lecture-07/README.md
@@ -0,0 +1,10 @@
+# lecture -05 Javascript Objects
+
+
+in this lecture we have studied about javascript Objects. what are object properties and methods, how we can define object, what are properrrtiess and methods, 
+how we can create object copy , how we can mute object properrrties. 
+
+
+# Assignment 
+
+Practice this at home.  
---
+++ lecture-07/script.js
@@ -0,0 +1,65 @@
+
+// const car = {
+//     name: 'Fiat',
+//     model: 2023,
+//     color: 'white',
+//     weight: 160
+// }
+
+// console.log(car.color)
+
+// const person = {};
+
+// add properties and values / key and values
+// person.firstName = 'Orhan';
+// person.lastName = 'Ghazi';
+// person.skinColor = 'white';
+// person.status = true;
+
+// console.log(person.lastName)
+
+// new keyword object creation
+
+// const car = new Object(
+//     {
+//         name: 'maroti',
+//         model: 2023,
+//         color: 'gray'
+//     }
+// );
+
+// car.name = "Carola";
+// car.model = 2023;
+// car.color = 'black';
+
+// console.log(car['model'])
+
+
+// simple function in js
+
+// function welcome(){
+//    return ( 'Ghazi');
+// }
+
+// let welcomeUSer = welcome()
+
+// console.log(welcomeUSer);
+
+
+// const person  = {
+//     name: 'Ghazi',
+//     lName: 'Orhan',
+//     edu: 'Msc',
+//     fullName: function (){
+//         return `${this.name} ${this.lName}`
+//     }
+// }
+
+// objects are mutable / creating a copy of object
+
+// const humaiza = person;
+// humaiza.name = "Humaiza";
+// humaiza.lName = "Waheed";
+
+
+// console.log(humaiza.fullName())
